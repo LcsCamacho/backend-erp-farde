a5833739cd9b8941d1c4883ae13108d07070dc11
Lucas A. Camacho <111870284+LcsCamacho@users.noreply.github.com>
2023-06-18 21:20:31 -0300

    fix

--- src/controller/funcionario.ts
+++ src/controller/funcionario.ts
@@ -2,23 +2,39 @@ import { Request, Response } from "express";
 import { prisma } from "../services/connect";
 
 export const listar = async (req: Request, res: Response) => {
-  const funcionarios = await prisma.funcionario.findMany();
-  res.json(funcionarios).status(200).end();
+  try {
+    const funcionarios = await prisma.funcionario.findMany();
+    if (funcionarios) res.json(funcionarios).status(200).end();
+    else throw new Error("Erro ao listar funcionarios");
+  } catch (error) {
+    console.log(error);
+    res
+      .json({
+        error: "Erro ao listar funcionarios",
+        log: JSON.stringify(error),
+        erro: true,
+      })
+      .status(500)
+      .end();
+  }
 };
 
 export const inserir = async (req: Request, res: Response) => {
-  const { nome, sac, cidade, pdv, processoConcluido, linkPerfil } = req.body;
-  const funcionario = await prisma.funcionario.create({
-    data: {
-      nome,
-      sac,
-      cidade,
-      pdv,
-      processoConcluido,
-      linkPerfil,
-    },
-  });
-  res.json(funcionario).status(200).end();
+  try {
+    const { nome, sac, cidade, pdv, processoConcluido, linkPerfil } = req.body;
+    const funcionario = await prisma.funcionario.create({
+      data: {
+        nome,
+        sac,
+        cidade,
+        pdv,
+        processoConcluido,
+        linkPerfil,
+      },
+    });
+    if (funcionario) res.json(funcionario).status(201).end();
+    else throw new Error("Erro ao inserir funcionario");
+  } catch (error) {}
 };
 export const inserirVarios = async (req: Request, res: Response) => {
   console.log(req.body);
@@ -41,48 +57,100 @@ export const inserirVarios = async (req: Request, res: Response) => {
   }
 };
 export const deletarVarios = async (req: Request, res: Response) => {
-  const funcionario = await prisma.funcionario.deleteMany();
-  res.json(funcionario).status(200).end();
+  try {
+    const funcionario = await prisma.funcionario.deleteMany();
+    if (funcionario) res.json(funcionario).status(200).end();
+    else throw new Error("Erro ao deletar funcionarios");
+  } catch (error) {
+    console.log(error);
+    res
+      .json({
+        error: "Erro ao deletar funcionarios",
+        log: JSON.stringify(error),
+        erro: true,
+      })
+      .status(500)
+      .end();
+  }
 };
 
 export const atualizar = async (req: Request, res: Response) => {
-  const { id, nome, sac, cidade, pdv, processoConcluido, linkPerfil } =
-    req.body;
-  const funcionario = await prisma.funcionario.update({
-    where: {
-      id: Number(id),
-    },
-    data: {
-      nome,
-      sac,
-      cidade,
-      pdv,
-      processoConcluido,
-      linkPerfil,
-    },
-  });
-  res.json(funcionario).status(200).end();
+  try {
+    const { id, nome, sac, cidade, pdv, processoConcluido, linkPerfil } =
+      req.body;
+    const funcionario = await prisma.funcionario.update({
+      where: {
+        id: Number(id),
+      },
+      data: {
+        nome,
+        sac,
+        cidade,
+        pdv,
+        processoConcluido,
+        linkPerfil,
+      },
+    });
+    if (funcionario) res.json(funcionario).status(201).end();
+    else throw new Error("Erro ao atualizar funcionario");
+  } catch (error) {
+    console.log(error);
+    res
+      .json({
+        error: "Erro ao atualizar funcionario",
+        log: JSON.stringify(error),
+        erro: true,
+      })
+      .status(500)
+      .end();
+  }
 };
 
 export const atualizarLinkPerfil = async (req: Request, res: Response) => {
-  const { id, linkPerfil } = req.body;
-  const funcionario = await prisma.funcionario.update({
-    where: {
-      id: Number(id),
-    },
-    data: {
-      linkPerfil,
-    },
-  });
-  res.json(funcionario).status(201).end();
+  try {
+    const { id, linkPerfil } = req.body;
+    const funcionario = await prisma.funcionario.update({
+      where: {
+        id: Number(id),
+      },
+      data: {
+        linkPerfil,
+      },
+    });
+    if (funcionario) res.json(funcionario).status(201).end();
+    else throw new Error("Erro ao atualizar link do perfil");
+  } catch (error) {
+    console.log(error);
+    res
+      .json({
+        error: "Erro ao atualizar link do perfil",
+        log: JSON.stringify(error),
+        erro: true,
+      })
+      .status(500)
+      .end();
+  }
 };
 
 export const deletar = async (req: Request, res: Response) => {
-  const { id } = req.params;
-  const funcionario = await prisma.funcionario.delete({
-    where: {
-      id: Number(id),
-    },
-  });
-  res.json(funcionario).status(200).end();
+  try {
+    const { id } = req.params;
+    const funcionario = await prisma.funcionario.delete({
+      where: {
+        id: Number(id),
+      },
+    });
+    if (funcionario) res.json(funcionario).status(200).end();
+    else throw new Error("Erro ao deletar funcionario");
+  } catch (error) {
+    console.log(error);
+    res
+      .json({
+        error: "Erro ao deletar funcionario",
+        log: JSON.stringify(error),
+        erro: true,
+      })
+      .status(500)
+      .end();
+  }
 };
